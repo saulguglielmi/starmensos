914653bd4103a9419b1b3e85fdb8be221f947948
saulguglielmi <saulguglielmi@gmail.com>
2025-10-17 19:54:25 -0600

    se añadieron posibles variables

---
+++ app.js
@@ -0,0 +1,6 @@
+const inputNombre = document.getElementById('nombre');
+const botonEjecutar = document.getElementById('buscar');
+const altura = document.getElementById('altura');
+const masa = document.getElementById('masa');
+const peliculas = document.getElementById('peliculas'); // cambie peliculas en vez de items
+
